5ec43784be0a7d9309da0155dd3a164a0401e1a7
Himanshu Prajapat <prajapathimanshu2006@gmail.com>
2026-07-20 12:15:30 +0530

    Time: 39 ms (6.58%), Space: 8.6 MB (50.23%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==1 || x==2 || x==3)
+        {
+            return 1;
+        }
+        if(x==4 || x==5)
+        {
+            return 2;
+        }
+        for(double i=0;i<x/2;i++)
+        {
+            if(x==i*i)
+            {
+                return i;
+            }
+            if(x>i*i && x<(i+1)*(i+1))
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+};
